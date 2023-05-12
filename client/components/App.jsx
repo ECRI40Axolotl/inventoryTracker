6e5a540c10718a5a1a444b28bdca9500fb6627ec
farrellkai <farrellkai@gmail.com>
2023-05-12 19:58:10 -0400

    built App.jsx file
    Co-authored-by: Kai
    Co-authored-by: Jeff

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Route } from 'react-router-dom';
+import './stylesheets/styles.css';
+import Fridge from './Fridge.jsx';
+
+function App() {
+  return (
+    <div className='fridge'>
+      <Fridge />
+    </div>
+  )
+}
+
+export default App;
