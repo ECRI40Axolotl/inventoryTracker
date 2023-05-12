f970c53b567ad6aed462e5c7bfab6093b5550dc5
Lucienne Seidler <lucienneseidler@luciennes-mbp.lan>
2023-05-12 18:58:37 -0400

    updated server error status

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use((req, res) => res.status(404).send('This page does not exist'));
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
-    status: 500,
+    status: 400,
     message: { err: 'An error occurred' },
   };
   const errorObj = Object.assign({}, defaultErr, err);
