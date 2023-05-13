58cbfab3f26ed1163be93ea43a9bf03484813aad
farrellkai <farrellkai@gmail.com>
2023-05-13 10:41:42 -0400

    rendering components to fridge

--- client/components/Fridge.jsx
+++ client/components/Fridge.jsx
@@ -3,29 +3,21 @@ import InventoryItem from './InventoryItem.jsx';
 
 function Fridge(props) {
   const [item, setItem] = useState('');
-    function componentDidMount() {
+    useEffect(() => {
       fetch('/fridge')
         .then(res => res.json())
         .then(data => {
             setItem(data);
         })
         .catch(err => {
-            console.log('Fridge.componentDidMount: get items: ERROR:, ', err);
+            console.log('Fridge.useEffect: get items: ERROR:, ', err);
         })
-    }
+    })
 
-
-    //useEffect
-    async function compon {
-        try {
-          const data = await fetch('/fridge');
-          await setItem(data.json());
-        } catch (err) {
-          console.log('Fridge.componentDidMount: get items: ERROR:, ', err);
-        }
-    }
 
   return (
-    
+    <div>
+      <InventoryItem />
+    </div>
   )
 }
