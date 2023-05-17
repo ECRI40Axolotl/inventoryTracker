02dc9232c0e52d64944c172c6f551088eb4befa7
Evram Dawd <evramdawd@evrams-mbp.myfiosgateway.com>
2023-05-17 14:47:28 -0400

    welcome page and stylesheet changes

--- client/pages/WelcomePage.jsx
+++ client/pages/WelcomePage.jsx
@@ -6,12 +6,18 @@ function WelcomePage() {
   return(
     <div className="welcomePage">
       <LogoHeader/>
+
       <Link className="welcomeLogIn" to="/loginPage">
           Log In
       </Link> &nbsp;
+
       <Link className="welcomeSignUp" to="/signupPage">
           Sign Up
       </Link>
+
+      <div className='fridge-icon-container'>
+        <img id="fridge-icon" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/79314/kitchen-clipart-xl.png" alt="fridge art"/>
+      </div>
 
     </div>
   );
