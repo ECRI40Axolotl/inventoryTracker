b4fb1d9e96d24cd6cebd45e66604be12202319b7
Lucienne Seidler <lucienneseidler@luciennes-mbp.lan>
2023-07-18 14:37:43 -0400

    client side major updates

--- client/index.js
+++ client/index.js
@@ -3,7 +3,17 @@ import { render } from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
-import styles from './stylesheets/styles.css';
+import './stylesheets/App.scss';
+import './stylesheets/_styles.scss';
 
 // render(<App />, document.getElementById('root'));
-createRoot(document.getElementById('app')).render(<App />);
+// createRoot(document.getElementById('app')).render(<App />);
+
+const domNode = document.getElementById('app');
+const root = createRoot(domNode);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
---
+++ client/pages/Landing.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+function WelcomePage() {
+  const navigate = useNavigate();
+  return (
+    <div className='welcomePage'>
+      <div className='signup-login-container'>
+        <Link className='welcomeLogIn' to='/login'>
+          Log In
+        </Link>{' '}
+        &nbsp;
+        <Link className='welcomeSignUp' to='/register'>
+          Sign Up
+        </Link>
+      </div>
+      <textbox
+        onClick={() => navigate('/login')}
+        className='fridge-icon-container'
+      >
+        <img
+          id='fridge-icon'
+          src='https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/79314/kitchen-clipart-xl.png'
+          alt='fridge art'
+        />
+      </textbox>
+    </div>
+  );
+}
+
+export default WelcomePage;
